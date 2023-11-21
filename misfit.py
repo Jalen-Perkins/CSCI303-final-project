2b688da02e31db741538a56faf845a51594e7ca0
Jalen-Perkins <Jperkins1@mines.edu>
2023-11-21 15:43:50 -0700

    fix indent in misfit.py

--- misfit.py
+++ misfit.py
@@ -10,24 +10,24 @@ def get_thetas(num=4):
 
 def dist(r,theta,rho=2.5):
 
-assert len(r) == len(theta[i]), 'r and theta should be the same length'
+    assert len(r) == len(theta[i]), 'r and theta should be the same length'
 
-verts=[]
+    verts=[]
 
-for i in range(len(r)):
-    x_i = r[i]*np.cos(theta[i])
-    z_i = r[i]*np.sin(theta[i])
+    for i in range(len(r)):
+        x_i = r[i]*np.cos(theta[i])
+        z_i = r[i]*np.sin(theta[i])
 
-    verts.append([x_i,z_i])
+        verts.append([x_i,z_i])
 
-grav_obs = pd.read_csv('data/tunnel.csv')
+    grav_obs = pd.read_csv('data/tunnel.csv')
 
 
-obs = grav_obs.index
+    obs = grav_obs.index
 
-grav_pred = gpoly(obs, verts, rho) # gravatational potental predicted by forward modeling
+    grav_pred = gpoly(obs, verts, rho) # gravatational potental predicted by forward modeling
 
-return np.sum(((grav_pred-grav_obs)/grav_obs)**2)
+    return np.sum(((grav_pred-grav_obs)/grav_obs)**2)
 
 
 
